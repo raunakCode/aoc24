5f22cf6dfdc203be69dca938e517702d0677cab1
Raunak Redkar <raunak.redkar@gmail.com>
2024-12-13 17:38:27 +0100

    there's no way I needed union find

--- day12/2.cpp
+++ day12/2.cpp
@@ -39,9 +39,15 @@ int main() {
 
 	vector<pair<ll, ll>> dirs = {{1, 0}, {-1, 0}};
 	vector<pair<ll, ll>> dirs2 = {{0, 1}, {0, -1}};
+
+	vector<pair<ll, ll>> up = {{-1, 0}};
+	vector<pair<ll, ll>> down = {{1, 0}};
+	vector<pair<ll, ll>> lt = {{0, -1}};
+	vector<pair<ll, ll>> rt = {{0, 1}};
+
 	vector<pair<ll, ll>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-	ll n = 4;
+	ll n = 140;
 	vector<vector<char>> grid(n, vector<char>(n));
 	rep(i, 0, n) rep(j, 0, n) {
 		cin >> grid[i][j];
@@ -57,18 +63,20 @@ int main() {
 
 	auto isEdge = [&](ll y, ll x, ll mode) -> bool {
 		ll c = grid[y][x];
-		if (mode == 1) swap(directions, dirs2);
-		if (mode == 2) swap(directions, dirs);
 		bool ret = 0;
-		for(auto &d: directions) {
+		vector<pair<ll, ll>> curDirections;
+		if (mode == 0) curDirections = directions;
+		if (mode == 1) curDirections = lt;
+		if (mode == 2) curDirections = rt;
+		if (mode == 3) curDirections = up;
+		if (mode == 4) curDirections = down;
+		for(auto &d: curDirections) {
 			ll Y = y + d.first, X = x + d.second;
 			if (inside(Y, X)) {
 				if (grid[Y][X] != c) ret = true;
 			}
 			else ret = true;
 		}
-		if (mode == 1) swap(directions, dirs2);
-		if (mode == 2) swap(directions, dirs);
 		return ret;
 	};
 
@@ -84,47 +92,54 @@ int main() {
 		}
 	}
 
-	UF vertical(n * n);
-	UF horizontal(n * n);
-	rep(k, 0, 2) {
-		if (k == 1) {
-			swap(vertical, horizontal);
-			swap(dirs, dirs2);
-		}
+	UF UP(n * n);
+	UF DOWN(n * n);
+	UF LEFT(n * n);
+	UF RIGHT(n * n);
+	UF uf(n * n);
+
+	rep(k, 1, 5) {
+		if (k >= 3) swap(dirs, dirs2);	
+		if (k == 1) swap(uf, LEFT);
+		if (k == 2) swap(uf, RIGHT);
+		if (k == 3) swap(uf, UP);
+		if (k == 4) swap(uf, DOWN);
 		rep(i, 0, n) rep(j, 0, n) {
 			ll cur = convert(i, j);
-			if (!isEdge(i, j, 0)) continue;
+			if (!isEdge(i, j, k)) continue;
 			for(auto &d: dirs) {
 				ll y = i + d.first, x = j + d.second;
 				ll neighbour = convert(y, x);
-				if (inside(y, x) && isEdge(y, x, k + 1)) {
-					vertical.unite(cur, neighbour);
+				if (inside(y, x) && grid[y][x] == grid[i][j] && isEdge(y, x, k)) {
+					uf.unite(cur, neighbour);
 				}
 			}
 		}
+		if (k >= 3) swap(dirs, dirs2);	
+		if (k == 1) swap(uf, LEFT);
+		if (k == 2) swap(uf, RIGHT);
+		if (k == 3) swap(uf, UP);
+		if (k == 4) swap(uf, DOWN);
 	}
-	swap(vertical, horizontal);
 	swap(dirs, dirs2);
 
-	map<ll, pair<unordered_set<ll>, unordered_set<ll>>> info;
+	map<ll, array<unordered_set<ll>, 4>> info;
 	rep(i, 0, n) rep(j, 0, n) {
-		if (isEdge(i, j, 0)) {
-			ll comp = full.get(convert(i, j));
-			pair<unordered_set<ll>, unordered_set<ll>> &sets = info[comp];
-			sets.first.insert(vertical.get(convert(i, j)));
-			sets.second.insert(horizontal.get(convert(i, j)));
-		}
+		ll comp = full.get(convert(i, j));
+		array<unordered_set<ll>, 4> &sets = info[comp];
+		if (isEdge(i, j, 1)) sets[0].insert(LEFT.get(convert(i, j)));
+		if (isEdge(i, j, 2)) sets[1].insert(RIGHT.get(convert(i, j)));
+		if (isEdge(i, j, 3)) sets[2].insert(UP.get(convert(i, j)));
+		if (isEdge(i, j, 4)) sets[3].insert(DOWN.get(convert(i, j)));
 	}
 
 	ll ans = 0;
-	dbg(sz(info));
 	for(auto &[comp, sets]: info) {
 		ll area = full.mass[comp];
-		ll perimeter = sz(sets.first) + sz(sets.second);
+		ll perimeter = 0;
+		rep(i, 0, 4) perimeter += sz(sets[i]);
 		ans += area * perimeter;
 	}
 
 	cout << ans;
 }
-
-
