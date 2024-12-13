b45917c807c2540ef4da12c06d934b8a1432b302
Raunak Redkar <raunak.redkar@gmail.com>
2024-12-13 16:33:00 +0100

    adding day8 solutions

---
+++ day8/1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	ll N = 50;
+	vector<vector<char>> grid(N, vector<char>(N));
+	map<char, vector<pll>> pos;
+	rep(i, 0, N) rep(j, 0, N) {
+		cin >> grid[i][j];
+		if (grid[i][j] != '.') {
+			pos[grid[i][j]].push_back({i, j});
+		}
+	}
+	set<pll> antinodes;
+	for(auto &x: pos) {
+		vector<pll> v = x.second;
+		rep(i, 0, sz(v)) {
+			rep(j, 0, sz(v)) {
+				if (i == j) continue;
+				ll dy = v[i].first - v[j].first;
+				ll dx = v[i].second - v[j].second;
+				ll Y = v[j].first + 2 * dy;
+				ll X = v[j].second + 2 * dx;
+				if (0 <= Y && Y < N && 0 <= X && X < N) {
+					antinodes.insert({Y, X});
+				}
+			}
+		}
+	}
+	cout << sz(antinodes);
+}
+
